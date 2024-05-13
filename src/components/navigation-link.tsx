02e9e4a164c4e8096163314c201dc38105cf989d
Miguel Nascimento <miguelgomes13@live.com>
2024-05-13 16:31:45 -0300

    chore: if its in same origin dont open in a new tab

--- src/components/navigation-link.tsx
+++ src/components/navigation-link.tsx
@@ -1,5 +1,6 @@
 import { cn } from '@/lib/utils'
 import Link from 'next/link'
+import { useMemo } from 'react'
 
 type NavigationLinkProps = {
   href: string
@@ -16,11 +17,20 @@ export default function NavigationLink({
   target = '_blank',
   className,
 }: NavigationLinkProps) {
+  const isSameOrigin = useMemo(() => {
+    try {
+      const url = new URL(href)
+      return url.origin === window.location.origin
+    } catch (error) {
+      return false
+    }
+  }, [href])
+
   return (
     <Link
       href={href}
-      target={target}
-      rel={rel}
+      target={isSameOrigin ? '_self' : target}
+      rel={isSameOrigin ? '' : rel}
       className={cn(
         'flex h-8 w-8 items-center justify-center rounded-lg transition-colors hover:bg-transparent-gray',
         className,
