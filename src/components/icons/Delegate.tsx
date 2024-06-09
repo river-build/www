4f0370dd4c6d810c9d951bb1446e739a4b2a2d0e
Miguel Nascimento <miguelgomes13@live.com>
2024-06-09 18:23:28 -0300

    stop-color -> stopColor

--- src/components/icons/Delegate.tsx
+++ src/components/icons/Delegate.tsx
@@ -16,9 +16,9 @@ export default function Delegate() {
           y2="0.704753"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#82E4A3" />
-          <stop offset="0.5" stop-color="#E48290" />
-          <stop offset="1" stop-color="#8C84F7" />
+          <stop stopColor="#82E4A3" />
+          <stop offset="0.5" stopColor="#E48290" />
+          <stop offset="1" stopColor="#8C84F7" />
         </linearGradient>
       </defs>
     </svg>
